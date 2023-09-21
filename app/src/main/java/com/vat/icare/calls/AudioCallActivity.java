7666dc042a71a9f49608a90688cb12751d291bee
Vatey <tabitzy@gmail.com>
2023-09-22 00:29:30 +0700

    UI Improvement

---
+++ app/src/main/java/com/vat/icare/calls/AudioCallActivity.java
@@ -0,0 +1,16 @@
+package com.vat.icare.calls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vat.icare.R;
+
+public class AudioCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_call);
+    }
+}
